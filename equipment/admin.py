aa556b18a5563868286212a47a5d428b36d8e5d8
Mateusz <mateoszzz@onet.pl>
2022-12-13 19:38:51 +0100

    Pierwszy zrzut plików

---
+++ equipment/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Sprzet, Kategoria, Producent, Lokalizacja
+
+admin.site.register(Sprzet)
+admin.site.register(Kategoria)
+admin.site.register(Producent)
+admin.site.register(Lokalizacja)
+
+
+
---
+++ equipment/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.utils import timezone
+
+class Kategoria(models.Model):
+    nazwa = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nazwa
+
+    class Meta:
+        verbose_name = "Kategoria"
+        verbose_name_plural = "Kategorie"
+
+class Producent(models.Model):
+    nazwa = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nazwa
+
+    class Meta:
+        verbose_name = "Producent"
+        verbose_name_plural = "Producenci"
+
+class Lokalizacja(models.Model):
+    nazwa = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nazwa
+
+    class Meta:
+        verbose_name = "Lokalizacja"
+        verbose_name_plural = "Lokalizacje"
+
+
+class Sprzet(models.Model):
+    kategoria = models.ForeignKey(Kategoria,on_delete=models.CASCADE, null=True, blank=False)
+    producent = models.ForeignKey(Producent, on_delete=models.CASCADE, null=True, blank=False)
+    nazwa = models.CharField(max_length=50, null=True, blank=False)
+    numer_seryjny = models.CharField(max_length=50, null=True, blank=False)
+    numer_inwentarzowy = models.CharField(max_length=6, null=True, blank=False)
+    lokalizacja = models.ForeignKey(Lokalizacja, on_delete=models.CASCADE, null=True, blank=False)
+    data_utworzenia = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nazwa
+
+    class Meta:
+        verbose_name = "Sprzet"
+        verbose_name_plural = "Sprzety"
+
+
