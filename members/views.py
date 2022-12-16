14558af1bb54a549001a10cc9e14f66b81407859
Mateusz <mateoszzz@onet.pl>
2022-12-16 22:23:36 +0100

    Dodanie logowania do apliakcji

--- members/views.py
+++ members/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 def login_user(request):
-        if request.method == "POST":
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('equipment_list')
-            else:
-                messages.success(request, ("Nieporawne dane logowania, spróbuj ponownie"))
-                return redirect('login_user')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('equipment_list')
         else:
-            return render(request, 'members/login.html', {})
+            messages.success(request, "Nieporawne dane logowania, spróbuj ponownie")
+            return redirect('login_user')
+    else:
+        return render(request, 'members/login.html', {})
