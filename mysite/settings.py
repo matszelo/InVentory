21275a68e257cef879a4e5c3a1b25cff1dcf2387
Mateusz <mateoszzz@onet.pl>
2022-12-26 23:28:21 +0100

    Zrobienie komentarza w STATICFILES_DIRS

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
