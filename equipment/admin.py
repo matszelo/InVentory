0748bf87fb1923f7786355d7f6399369e2f627b1
Mateusz <mateoszzz@onet.pl>
2022-12-26 21:55:19 +0100

    Usunąłem grupy z panelu admina

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Sprzet, Kategoria, Producent, Lokalizacja
+from django.contrib.auth.models import Group
 
+admin.site.unregister(Group)
 admin.site.register(Sprzet)
 admin.site.register(Kategoria)
 admin.site.register(Producent)
