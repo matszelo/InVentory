62901b912b7949d89a8595de507c01c566cfcbc3
STONiczek <120453101+STONiczek@users.noreply.github.com>
2023-01-27 08:54:43 +0100

    Update views.py
    Dodanie komentarzy

--- equipment/views.py
+++ equipment/views.py
@@ -11,6 +11,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
 
+# Generownie pliku PDF
 
 def equipment_pdf(request):
     buf = io.BytesIO()
@@ -43,6 +44,7 @@ def equipment_pdf(request):
 
     return FileResponse(buf, as_attachment=True, filename='Lista_sprzetow.pdf')
 
+# GEnerowanie pliku CSV
 
 def equipment_csv(request):
     response = HttpResponse(content_type='text/csv')
@@ -59,37 +61,42 @@ def equipment_csv(request):
 
     return response
 
+# Wyszukiwanie sprzętu w bazie
 
 def search_equipment(request):
     if request.method == "POST":
         searched = request.POST['searched']
-        equipments = Sprzet.objects.filter(nazwa__icontains=searched)
+        equipments = Sprzet.objects.filter(nazwa__icontains=searched) # Filtr wyszukiwania sprzętu po nazwie
         return render(request, 'equipment/search_equipment.html', {'searched': searched, 'equipments': equipments})
     else:
         return render(request, 'equipment/search_equipment.html', {})
 
+# Usuwanie sprzętu z bazy    
 
 def delete_equipment(request, equipment_id):
     equipment = Sprzet.objects.get(pk=equipment_id)
     equipment.delete()
-    messages.success(request, "Poprawnie usunięto sprzęt")
+    messages.success(request, "Poprawnie usunięto sprzęt") # Komunikat po usunięciu sprzętu
     return redirect('equipment_list')
 
+# Aktualizowanie danych sprzętu w bazie
 
 def update_equipment(request, equipment_id):
     equipment = Sprzet.objects.get(pk=equipment_id)
     form = Equipmentform(request.POST or None, request.FILES or None, instance=equipment)
     if form.is_valid():
         form.save()
-        messages.success(request, "Poprawnie zapisano dane sprzętu")
+        messages.success(request, "Poprawnie zapisano dane sprzętu") # Komunikat po zaktualizowaniu sprzętu
         return redirect('equipment_list')
     return render(request, 'equipment/update_equipment.html', {'equipment': equipment, 'form': form})
 
+# Wyświetlenie danego sprzętu z bazy
 
 def show_equipment(request, equipment_id):
     equipment = Sprzet.objects.get(pk=equipment_id)
     return render(request, 'equipment/show_equipment.html', {'equipment': equipment})
 
+# Dodanie sprzętu do bazy
 
 def add_equipment(request):
     submitted = False
@@ -97,7 +104,7 @@ def add_equipment(request):
         form = Equipmentform(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, "Poprawnie dodano nowy sprzęt")
+            messages.success(request, "Poprawnie dodano nowy sprzęt") # KOmunikat po dodaniu sprzętu
             return redirect('equipment_list')
     else:
         form = Equipmentform
@@ -105,14 +112,15 @@ def add_equipment(request):
             submitted = True
     return render(request, 'equipment/add_equipment.html', {'form': form, 'submitted': submitted})
 
+# Wyświetlenie wszystkich sprzętów w bazie
 
 def all_equipment(request):
     equipment_count = Sprzet.objects.all().count()
     equipment_list = Sprzet.objects.all()
-    p = Paginator(Sprzet.objects.all(), 10)
+    p = Paginator(Sprzet.objects.all(), 10) # Ograniczenie ilości sprzętu do 10 na jednej stronie
     page = request.GET.get('page')
     equipments = p.get_page(page)
-    nums = "a" * equipments.paginator.num_pages
+    nums = "a" * equipments.paginator.num_pages # Numeracja stron sprzętu
     return render(request, 'equipment/equipment_list.html',
                   {'equipment_list': equipment_list, 'equipments': equipments, 'nums': nums,
                    'equipment_count': equipment_count})
